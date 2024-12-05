c15d8e0d102ceb722a08345f868fd7efec2d0d63
Sampark Sharma <25999504+Phantsure@users.noreply.github.com>
2024-12-05 16:07:13 +0530

    Update GetUserByUsername.cs

--- IndividualChallenges/GetUserByUsername.cs
+++ IndividualChallenges/GetUserByUsername.cs
@@ -1,14 +1,14 @@
 public void GetUserByUsername(string username)
 {
-    // SQL injection vulnerability
     string connectionString = "YourConnectionStringHere";
-    string query = "SELECT * FROM Users WHERE Username = '" + username + "'";
+    string query = "SELECT * FROM Users WHERE Username = @username";
 
     using (var connection = new SqlConnection(connectionString))
     {
         connection.Open();
         using (var command = new SqlCommand(query, connection))
         {
+            command.Parameters.AddWithValue("@username", username);
             using (var reader = command.ExecuteReader())
             {
                 while (reader.Read())
